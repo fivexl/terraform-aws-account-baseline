354d71da7bb896339de4d2fb14115171cd385b30
Anton Eremin <mobessona2@gmail.com>
2023-12-13 13:36:59 +0500

    feat: s3_baseline module

---
+++ modules/s3_baseline/main.tf
@@ -0,0 +1,38 @@
+module "bucket_baseline" {
+  count   = var.create_bucket ? 1 : 0
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+
+  bucket = var.bucket_name
+
+  versioning                           = var.versioning
+  server_side_encryption_configuration = var.server_side_encryption_configuration
+
+  block_public_acls                 = true
+  block_public_policy               = true
+  ignore_public_acls                = true
+  restrict_public_buckets           = true
+  control_object_ownership          = var.control_object_ownership
+  object_ownership                  = var.object_ownership
+  attach_access_log_delivery_policy = var.attach_access_log_delivery_policy
+
+  tags = var.tags
+}
+
+# Logging done via resource, because for now module does not support new Date-based partitioning feature
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_logging#target_object_key_format
+# 2023.12.11
+resource "aws_s3_bucket_logging" "this" {
+  count = var.create_bucket && length(var.logging.target_bucket) != "" ? 1 : 0
+
+  bucket = module.bucket_baseline[0].s3_bucket_id
+
+  target_bucket = var.logging.target_bucket
+  target_prefix = try(var.logging.target_prefix, null)
+
+  target_object_key_format {
+    partitioned_prefix {
+      partition_date_source = "EventTime"
+    }
+  }
+}
---
+++ modules/s3_baseline/variables.tf
@@ -0,0 +1,63 @@
+variable "create_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, module will create S3 bucket, with predefined best practices."
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the bucket."
+}
+
+variable "logging" {
+  type = object(
+    {
+      target_bucket = optional(string, "")
+      target_prefix = optional(string)
+    }
+  )
+  default     = {}
+  description = "Logging settings for the S3 bucket."
+}
+
+variable "versioning" {
+  type        = any
+  default     = { enabled = true }
+  description = "Enable versioning for the S3 bucket."
+}
+
+variable "server_side_encryption_configuration" {
+  type = any
+  default = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  description = "The server-side encryption algorithm to use for the S3 bucket."
+}
+
+variable "control_object_ownership" {
+  type        = bool
+  default     = true
+  description = "Control object ownership for the bucket."
+}
+
+variable "object_ownership" {
+  type        = string
+  default     = "BucketOwnerEnforced"
+  description = "The type of object ownership for the bucket."
+}
+
+variable "attach_access_log_delivery_policy" {
+  type        = bool
+  default     = false
+  description = "Attach access log delivery policy to the bucket."
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags to be applied to the bucket."
+}
