3e0a8dc6a3e8df09f77ce1ba07d2b87b3fca4be9
Anton Eremin <mobessona2@gmail.com>
2023-12-14 12:40:48 +0500

    feat: add s3_bucket_arn s3_bucket_id outputs

---
+++ modules/s3_baseline/outputs.tf
@@ -0,0 +1,9 @@
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket used for storing Terraform state"
+  value       = try(module.bucket_baseline[0].s3_bucket_arn, "")
+}
+
+output "s3_bucket_id" {
+  description = "The ARN of the S3 bucket used for storing Terraform state"
+  value       = try(module.bucket_baseline[0].s3_bucket_id, "")
+}
