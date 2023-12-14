560d8f937c6c79fcb05993ed85bb0f197907d2c9
Anton Eremin <mobessona2@gmail.com>
2023-12-14 11:07:39 +0500

    fix: lenght to length

--- modules/region_level/locals.tf
+++ modules/region_level/locals.tf
@@ -9,5 +9,5 @@ locals {
     target_bucket = var.create_s3_access_logs_bucket ? (var.s3_access_logs_bucket_name != "" ? var.s3_access_logs_bucket_name : local.s3_access_logs_bucket_name) : null
     target_prefix = var.create_s3_access_logs_bucket ? local.state_bucket_name : null
   }
-  state_logging_configuration = lenght(var.s3_tf_state_bucket_logging) > 0 ? var.s3_tf_state_bucket_logging : local.default_logging_configuration
+  state_logging_configuration = length(var.s3_tf_state_bucket_logging) > 0 ? var.s3_tf_state_bucket_logging : local.default_logging_configuration
 }
