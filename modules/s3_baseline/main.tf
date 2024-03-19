045339032d9040bcb2e874bf14ade6a13dd18420
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2024-03-19 17:19:38 +0000

    missing NLB logs delivery policy
    Signed-off-by: Andrey Devyatkin <andrey.devyatkin@fivexl.io>

--- modules/s3_baseline/main.tf
+++ modules/s3_baseline/main.tf
@@ -29,7 +29,8 @@ module "bucket_baseline" {
 
 
   attach_access_log_delivery_policy        = var.attach_access_log_delivery_policy
-  attach_elb_log_delivery_policy           = var.attach_elb_access_logs_policy
+  attach_elb_log_delivery_policy           = var.attach_access_log_delivery_policy
+  attach_lb_log_delivery_policy            = var.attach_access_log_delivery_policy
   attach_deny_insecure_transport_policy    = true
   attach_deny_incorrect_encryption_headers = true
   attach_deny_unencrypted_object_uploads   = true
