63c8044637c505d3511b6104174f8472c70e5348
Anton Eremin <mobessona2@gmail.com>
2024-10-22 14:16:29 +0500

    fix: correct output description

--- modules/s3_baseline/outputs.tf
+++ modules/s3_baseline/outputs.tf
@@ -4,6 +4,6 @@ output "s3_bucket_arn" {
 }
 
 output "s3_bucket_id" {
-  description = "The ARN of the S3 bucket used for storing Terraform state"
+  description = "The Name of the S3 bucket used for storing Terraform state"
   value       = try(module.bucket_baseline[0].s3_bucket_id, "")
 }
