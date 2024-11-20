e834dbef3123f1dd417b5f8bc92bef289d2fd382
Anton <125114167+EreminAnton@users.noreply.github.com>
2024-11-20 15:18:11 +0500

    fix: use correct naming conventions module source path

--- modules/athena_regional_baseline/data.tf
+++ modules/athena_regional_baseline/data.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-module "naming_convetions" {
-  source  = "fivexl/naming-convetions/aws"
+module "naming_conventions" {
+  source  = "fivexl/naming-conventions/aws"
   version = "0.0.1"
 }
--- modules/athena_regional_baseline/locals.tf
+++ modules/athena_regional_baseline/locals.tf
@@ -1,4 +1,4 @@
 locals {
   s3_access_logs_glue_table_name   = var.s3_access_logs_glue_table.name != null ? var.s3_access_logs_glue_table.name : "s3_server_access_logs"
-  athena_query_results_bucket_name = var.athena_query_results_bucket_name != "" ? var.athena_query_results_bucket_name : module.naming_convetions.athena_query_results_bucket_name
+  athena_query_results_bucket_name = var.athena_query_results_bucket_name != "" ? var.athena_query_results_bucket_name : module.naming_conventions.athena_query_results_bucket_name
 }
